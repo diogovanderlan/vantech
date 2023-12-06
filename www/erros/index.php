7ab1ac6da9dfcad61e7025e7c543b86192090bd5
Diogo Vanderlan <diogo.vanderlan@gmail.com>
2023-12-05 23:29:16 -0300

    site
    vantech site

---
+++ www/erros/index.php
@@ -0,0 +1,464 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; " charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Vantech</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+
+  <link href="assets/img/logo1.png" rel="icon">
+  <link href="assets/img/logo2.png" rel="apple-touch-icon">
+
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+  
+ 
+</head>
+
+<body>
+
+
+  <header id="header" class="fixed-top header-inner-pages">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="index.php">VanTech</a></h1>
+      
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">Sobre</a></li>
+          <li><a class="nav-link scrollto" href="#services">Serviços</a></li>
+          <li><a class="nav-link  scrollto" href="https://diogovanderlan.com.br">CEO</a></li>
+          <li><a class="nav-link scrollto" href="https://github.com/diogovanderlan">GitHub</a></li>
+          <li class="dropdown"><a href="#"><span>linguagens</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="https://www.php.net/manual/pt_BR/intro-whatis.php">PHP</a></li>
+              <li class="dropdown"><a href="#"><span>APP</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="https://flutter.dev/">Flutter</a></li>
+                  <li><a href="https://www.apple.com/br/swift/">Swifit</a></li>
+                  
+                </ul>
+              </li>
+              <li><a href="https://python.org.br/">Python</a></li>
+              <li><a href="https://aws.amazon.com/pt/what-is/javascript/#:~:text=O%20JavaScript%20surgiu%20como%20uma,layout%20do%20conte%C3%BAdo%20na%20p%C3%A1gina.">JavaScript</a></li>
+             
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto"  href="mailto:newvantech@gmail.com">Contato</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+    </div>
+  </header>
+
+ 
+  <section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+      <div class="carousel-inner" role="listbox">
+
+        
+        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 class="animated fadeInDown">SEJA BEM-VINDO <span>VANTECH</span></h2>
+              <p class="animated fadeInUp">O site da sua empresa precisa ser muito mais que um simples cartão de visitas online, deve ser uma maneira de potencializar as vendas e fortalecer sua marca..</p>
+              <a href="#about" class="btn-get-started animated fadeInUp scrollto">Sobre</a>
+            </div>
+          </div>
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-2.jpg)">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 class="animated fadeInDown">DESENVOLVIMENTO DE SITES RESPONSIVOS, RÁPIDOS E ELEGANTES</h2>
+              <p class="animated fadeInUp">DESENVOLVIMENTO DE SITES RESPONSIVOS, RÁPIDOS E ELEGANTES
+                Acreditamos que o digital requer uma visão ampla, por isso combinamos ideias e criatividade com tecnologia e marketing para alcançar resultados incríveis.
+                
+              </p>
+              <a href="#about" class="btn-get-started animated fadeInUp scrollto">Sobre</a>
+            </div>
+          </div>
+        </div>
+
+        <!-- Slide 3 -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg)">
+          <div class="carousel-container">
+            <div class="container">
+              <h2 class="animated fadeInDown">PROJETOS PERSONALIZADOS</h2>
+              <p class="animated fadeInUp">Não importa que tipo de site você precisa, nós temos a equipe ideal para gerar mais resultados para o seu negócio na Internet.
+
+                Desenvolvimento de Sites, Hotsites, Landing Pages, Blogs, Portais e Catálogos.
+                
+                Integração com Redes Sociais, Google Maps, Waze, WhatsApp, PagSeguro, PayPal e muito mais.</p>
+              <a href="#about" class="btn-get-started animated fadeInUp scrollto">Sobre</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+      </a>
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+  </section>
+
+  <main id="main">
+
+    
+    <section id="about" class="about">
+      <div class="container-fluid">
+
+        <div class="row justify-content-center">
+          <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch position-relative">
+            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox play-btn mb-4"></a>
+          </div>
+
+          <div class="col-xl-5 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
+            <h3>DIFERENCIAIS DE NOSSOS SITES</h3>
+            <p>Sites rápidos de verdade, que funcionam inclusive em conexões lentas sem a perda de usabilidade.</p>
+
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-fingerprint"></i></div>
+              <h4 class="title"><a href="">ALTA PERFORMANCE</a></h4>
+              <p class="description">Sites rápidos de verdade, que funcionam inclusive em conexões lentas sem a perda de usabilidade.</p>
+            </div>
+
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-gift"></i></div>
+              <h4 class="title"><a href="">SITE OTIMIZADO</a></h4>
+              <p class="description">Utilizamos técnicas de SEO para preparar o seu site para os buscadores e melhorar a relevância do seu conteúdo.</p>
+            </div>
+
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-atom"></i></div>
+              <h4 class="title"><a href="">SITE DINÂMICO</a></h4>
+              <p class="description">Veja as estatísticas de acesso, usuários inscritos e mensagens recebidas através do painel administrativo.</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    
+    <section id="skills" class="skills">
+      <div class="container-fluid">
+
+        <div class="row justify-content-center skills-content">
+
+          <div class="col-xl-5 col-lg-6">
+
+            <div class="progress">
+              <span class="skill">HTML <i class="val">100%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">CSS <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">JavaScript <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-xl-5 col-lg-6">
+
+            <div class="progress">
+              <span class="skill">PHP <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Flutter <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Python <i class="val">55%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    <section id="counts" class="counts section-bg">
+      <div class="container-fluid">
+
+        <div class="row counters">
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="25" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Clientes</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="92" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Projetos</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="620" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Horas de suporte</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-purecounter-start="0" data-purecounter-end="3" data-purecounter-duration="1" class="purecounter"></span>
+            <p>Trabalhadores esforçados</p>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    <section id="services" class="services">
+      <div class="container-fluid">
+
+        <div class="section-title">
+          <h2>Serviços</h2>
+          <h3>Confira nosso <span>Serviços</span></h3>
+          <p></p>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-xl-10">
+            <div class="row">
+              <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon"><i class="ri-pie-chart-line"></i></div>
+                <h4 class="title"><a href="">Tempo</a></h4>
+                <p class="description">Cada equipe desenvolve apenas um projeto por vez para que o cliente tenha seu projeto o mais rapido possivel  </p>
+              </div>
+              <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon"><i class="ri-stack-line"></i></div>
+                <h4 class="title"><a href="">Multi plataformas</a></h4>
+                <p class="description">Funcionamento ambientado para diversos canais diferentes. Como por exemplo, um sistema que opera no Windows, Android (ou iOs) e na nuvem ao mesmo tempo</p>
+              </div>
+              <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon"><i class="ri-markup-line"></i></div>
+                <h4 class="title"><a href="">Projetos personalizados</a></h4>
+                <p class="description">O cliente escolhe como deseja cada item do site ou do aplicativo</p>
+              </div>
+              <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon"><i class="ri-shape-line"></i></div>
+                <h4 class="title"><a href="">Sites Responsivos</a></h4>
+                <p class="description">Sites responsivos são aqueles que adaptam o tamanho das suas páginas (alteração do layout) ao tamanho das telas que estão sendo exibidos, como as telas de celulares e tablets. Suas vantagens derivam da adaptação a qualquer ferramenta que os usuários estejam usando para facilitar a sua visualização.</p>
+              </div>
+              <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon"><i class="ri-fingerprint-line"></i></div>
+                <h4 class="title"><a href="">Sites seguros e protegidos</a></h4>
+                <p class="description">Nós garantimos que seu site esteja sempre protegido contra malwares ou bugs. Seguimos as melhores práticas de desenvolvimento e ferramentas para garantia da qualidade do código.</p>
+              </div>
+              <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon"><i class="ri-body-scan-line"></i></div>
+                <h4 class="title"><a href="">Visibilidade</a></h4>
+                <p class="description">
+                  Através de uma estratégia de marketing digital bem definida, qualquer negócio pode fazer frente aos seus concorrentes, independente do seu tamanho.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    
+    <section id="cta" class="cta">
+      <div class="container">
+
+        <div class="text-center">
+          <h3>Qual a importância de um site</h3>
+          <p> O site é um elemento crucial para gerar resultados na internet. É o local de visita dos seus clientes, antes que eles decidam fechar negócio, e é o centralizador das estratégias de comunicação..</p>
+          <a class="cta-btn" href="https://olhardigital.com.br/2021/08/26/guia-do-empreendedor/entenda-a-importancia-de-ter-um-bom-site-com-um-design-profissional/">Mais Sobre</a>
+        </div>
+
+      </div>
+    </section>
+
+    <section id="team" class="team">
+      <div class="container-fluid">
+
+        <div class="section-title">
+          <h2>EQUIPE</h2>
+          <h3><span>CEO</span></h3>
+          <p></p>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-xl-10">
+            <div class="row">
+
+              <div class="center">
+                <div class="member">
+                  <img src="assets/img/team/eu3.jpg" class="img-fluid" alt="">
+                  <div class="member-info">
+                    <div class="member-info-content">
+                      <h4>Diogo Vanderlan</h4>
+                      <span>Diretor Executivo</span>
+                    </div>
+                    <div class="social">
+                      <a href="https://twitter.com/diogovanderlan"><i class="bi bi-twitter"></i></a>
+                      <a href="https://www.facebook.com/diogovanderlan/"><i class="bi bi-facebook"></i></a>
+                      <a href="https://www.instagram.com/diogovanderlan/"><i class="bi bi-instagram"></i></a>
+                      <a href="https://br.linkedin.com/in/diogovanderlan"><i class="bi bi-linkedin"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div> 
+
+              
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+   
+    <section id="contact" class="contact section-bg">
+      <div class="container-fluid">
+
+        <div class="section-title">
+          <h2>Contato</h2>
+          <h3>Entre em contato!<span></span></h3>
+          <p>Faça um orçamento.</p>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-xl-10">
+            <div class="row">
+
+              <div class="col-lg-6">
+
+                <div class="row justify-content-center">
+
+                  <div class="col-md-6 info d-flex flex-column align-items-stretch">
+                    <i class="bx bx-map"></i>
+                    <h4>Endereço</h4>
+                    <p>Av Brasil<br>Alto Piquiri, N° 1680</p>
+                  </div>
+                  <div class="col-md-6 info d-flex flex-column align-items-stretch">
+                    <i class="bx bx-phone"></i>
+                    <h4>Ligue para nós</h4>
+                    <p><br>+55 44 98839 4805</p>
+                  </div>
+                  <div class="col-md-6 info d-flex flex-column align-items-stretch">
+                    <i class="bx bx-envelope"></i>
+                    <h4>Email</h4>
+                    <p><br>newvantech@gmail.com</p>
+                  </div>
+                  <div class="col-md-6 info d-flex flex-column align-items-stretch">
+                    <i class="bx bx-time-five"></i>
+                    <h4>Jornada de trabalho</h4>
+                    <p>Seg - Sex: 18:00 as 23:00<br></p>
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div class="col-lg-6">
+                
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+  </main>
+
+  <footer id="footer">
+
+    <div class="footer-newsletter">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <h4>Não fique de fora.</h4>
+            <p>Entre em contato.</p>
+          </div>
+          <div class="col-lg-6">
+            <form action="https://formspree.io/f/mrgvjkqo" method="post">
+              <input type="email" name="email"><input type="submit" value="Enviar">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Criado por: <strong><span>Vantech</span></strong>.Todos os direitos reservados
+      </div>
+      <div class="credits">
+    
+        Projetado por <a href="https://diogovanderlan.com.br/">Diogo Vanderlan</a>
+      </div>
+    </div>
+  </footer>
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
